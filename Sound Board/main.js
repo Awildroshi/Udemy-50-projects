b82fb9d98d462a1575695efc0ae1b35a72d7f293
Awildroshi <151027830+Awildroshi@users.noreply.github.com>
2024-12-12 23:43:24 +0530

    Add files via upload

---
+++ Sound Board/main.js
@@ -0,0 +1,25 @@
+const sounds= ['Amongus', 'Let-her-go', 'Spiderman', 'Pedro', 'Takeoff']
+
+sounds.forEach(sound=>{
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', ()=>{
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs(){
+    sounds.forEach(sound=>{
+        const song= document.getElementById(sound)
+
+        song.pause()
+        song.currentTime=0;
+    })
+}
